9013b7fd78de04280c9b91d786918522adb98b1f
Tim Jones <timothy.jones@gmx.us>
2022-08-07 16:15:17 -0400

    getting project CRUDs extracted and working to DB

--- projectService/routes/project_routes.js
+++ projectService/routes/project_routes.js
@@ -1,47 +1,28 @@
 const express = require("express");
-const Project = require("../models/project");
+
+const {
+   getProjects,
+   getProjectById,
+   createProject,
+   deleteProject,
+   updateProject,
+} = require("../crud/projectCRUD");
+
 const router = express.Router();
 
 //Get All Projects----------------------------------------------------------
-router.get("/", (req, res) => {
-   res.json({ mssg: "GET All Projects!" });
-});
+router.get("/", getProjects);
 
 //Get Single Project----------------------------------------------------------
-router.get("/:id", (req, res) => {
-   
-   const id = req.params.id;
-   res.json({ mssg: `Getting single Project ${id}` });
-});
+router.get("/:id", getProjectById);
 
 //POST New Project----------------------------------------------------------
-router.post("/", async (req, res) => {
-   const { title, details, public, goalCompletionDate } = req.body;
-
-   try {
-      const project = await Project.create({
-         title,
-         details,
-         public,
-         goalCompletionDate,
-      });
-      res.status(200).json(project);
-   } catch (error) {
-      res.status(400).json({ error: error.message });
-      console.log(error);
-   }
-});
+router.post("/", createProject);
 
 //DELETE Project----------------------------------------------------------
-router.delete("/:id", (req, res) => {
-   const id = req.params.id;
-   res.json({ mssg: `Deleting Project ${id}` });
-});
+router.delete("/:id", deleteProject);
 
 //PATCH Project----------------------------------------------------------
-router.patch("/:id", (req, res) => {
-   const id = req.params.id;
-   res.json({ mssg: `Updating Project ${id}` });
-});
+router.patch("/:id", updateProject);
 
 module.exports = router;
