9dd5949a8e29f4bed353a6e80ad2c2b37410fd9c
Tim Jones <timothy.jones@gmx.us>
2022-08-08 02:25:47 -0400

    Setting up CORS services, beginning react layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // Pages & Components
 import Home from "./pages/Home";
+import SideNav from "./components/Nav";
+import TopBar from "./components/TopBar";
 
 function App() {
    return (
-      <div className="App">
-         <BrowserRouter>
-            <div className="pages">
-               <Routes>
-                  <Route path="/" element={<Home />} />
-               </Routes>
-            </div>
-         </BrowserRouter>
-      </div>
+      <main className="flex">
+         <SideNav />
+         <div className="w-[100%] flex flex-col bg-sitelightgray md:ml-[20%] xl:ml-[250px] dark:bg-sitedarkblue dark:text-sitelightgray mx-auto">
+            <BrowserRouter>
+               <TopBar />
+               <div className=" flex text-center mx-auto p-5">
+                  <Routes>
+                     <Route path="/" element={<Home />} />
+                  </Routes>
+               </div>
+            </BrowserRouter>
+         </div>
+      </main>
    );
 }
 
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-   return <div>HomePage</div>;
+   const [projects, setProjects] = useState(null);
+
+   useEffect(() => {
+      const fetchProjects = async () => {
+         const response = await fetch("http://localhost:4000/api/projects/");
+         const json = await response.json();
+
+         if (response.ok) {
+            setProjects(json);
+         }
+      };
+      fetchProjects();
+   }, []);
+
+   return (
+      <div className="flex flex-col bg-base-100 text-center">
+         {projects &&
+            projects.map((project) => (
+               <div key={project._id} className="p-4 m-2 border ">
+                  <div className="flex flex-col">
+                     <div>
+                        <b>Id: </b> {project._id}
+                     </div>
+                     <div>
+                        <b>Title: </b> {project.title}
+                     </div>
+                     <div>
+                        <b>Details: </b> {project.details}
+                     </div>
+                     <div>
+                        <b>Created: </b> {project.createdAt}
+                     </div>
+                  </div>
+               </div>
+            ))}
+      </div>
+   );
 };
 
 export default Home;
