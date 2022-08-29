c8ca9864c931160df5893a15f3e496859ef15cb7
Tim Jones <62278428+TimJones7@users.noreply.github.com>
2022-08-29 16:57:45 -0400

    Update comment_routes.js

--- commentService/routes/comment_routes.js
+++ commentService/routes/comment_routes.js
@@ -10,19 +10,19 @@ const {
 
 const router = express.Router();
 
-//Get All Comments-------------------------------------------------------
+//Get All Comments------------------------------------------------------
 router.get("/", getComments);
 
-//Get Single Comment-----------------------------------------------------
+//Get Single Comment----------------------------------------------------
 router.get("/:id", getCommentById);
 
-//POST New Comment-------------------------------------------------------
+//POST New Comment------------------------------------------------------
 router.post("/", createComment);
 
-//DELETE Comment---------------------------------------------------------
+//DELETE Comment--------------------------------------------------------
 router.delete("/:id", deleteComment);
 
-//PATCH Project----------------------------------------------------------
+//PATCH Project---------------------------------------------------------
 router.patch("/:id", updateComment);
 
 module.exports = router;
